a33f8e127dca99c5ee9ea65de74eb4a1c42a4625
min kim <minsik.kim@livestation.com>
2013-04-29 23:00:04 +0100

    weather id added

--- src/main/java/com/ifactory/service/weatherserver/WeatherFormatter.java
+++ src/main/java/com/ifactory/service/weatherserver/WeatherFormatter.java
@@ -4,8 +4,9 @@ import com.ifactory.client.openweather.Result;
 
 public class WeatherFormatter {
   public String json(Result result) {
-    return "{\"name\":" + "\"" + result.getName() + 
-	    "\",\"desc\":" + "\"" + result.getWeathers().get(0).getDescription() + 
+    return "{\"name\":" + "\"" + result.getName() + "\",\"weatherId\":" +
+      result.getWeathers().get(0).getId() + 
+	    ",\"desc\":" + "\"" + result.getWeathers().get(0).getDescription() + 
 	    "\",\"temp\":" + result.getTemp() + ",\"high\":" +  
 	    result.getTempMax() + ",\"low\":" +  
 	    result.getTempMin() + "}";
